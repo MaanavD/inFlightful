58f7875ef086237e6d8da3f60c9ee2868c848e0d
Jay Mody <jaykmody@gmail.com>
2019-10-26 21:49:12 -0400

    accept and return on tweet to tweet basis

--- backend/main.py
+++ backend/main.py
@@ -4,25 +4,12 @@ from sentiment import SentimentAnalyzer
 
 app = Flask(__name__)
 
-@app.route("/text_analysis", methods=['POST'])
-def entry():
+@app.route("/sentiment", methods=['POST'])
+def count_sentiment():
     data = json.loads(request.get_data().decode('utf-8'))
 
     analyzer = SentimentAnalyzer()
-    sentiment = analyzer.analyze(data["content"])
+    return jsonify(analyzer.analyze(data["tweet"]))
 
-    response = {"content": data["content"], "sentiment": 0}
-    if sentiment["magnitude"] < 1.0:
-        pass
-    elif sentiment["score"] > 0.2:
-        response["sentiment"] = 3
-    elif sentiment["score"] < -0.2:
-        response["sentiment"] = 1
-    else:
-        response["sentiment"] = 2
-
-    print("--- text_analysis ---")
-    print(sentiment["content"])
-    print("score: ", sentiment["score"], "\tmagnitude: ", sentiment["magnitude"], "\tsentiment:", response["sentiment"])
-
-    return jsonify(response)
+if __name__ == "__main__":
+    app.run(debug=True)
--- backend/sentiment.py
+++ backend/sentiment.py
@@ -8,15 +8,25 @@ class SentimentAnalyzer():
         self.language = "en"
         self.encoding_type = enums.EncodingType.UTF8
 
-    def analyze(self, content):
+    def analyze(self, text):
         document = {
-            "content": content,
+            "content": text,
             "type": self.type,
             "language": self.language
         }
         response = self.client.analyze_sentiment(document, encoding_type=self.encoding_type)
-        return {
-            "content": content,
-            "score": response["document_sentiment"]["score"],
-            "magnitude": response["document_sentiment"]["magnitude"]
+        response = {
+            "score": response.document_sentiment.score,
+            "magnitude": response.document_sentiment.magnitude
         }
+
+        if response["magnitude"] < 0.075:
+            response["sentiment"] = 0
+        elif response["score"] > 0.2:
+            response["sentiment"] = 3
+        elif response["score"] < -0.2:
+            response["sentiment"] = 1
+        else:
+            response["sentiment"] = 2
+
+        return response
